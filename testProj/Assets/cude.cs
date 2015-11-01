1936c9669eff969bd6d612a9bea58da7e58e0bac
Vipcovich <vipcovich@gmail.com>
2015-11-01 12:16:15 +0300

    xxx

---
+++ testProj/Assets/cude.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class cude : MonoBehaviour {
+
+	float elapsed;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		elapsed += Time.deltaTime;
+		if(elapsed >= 1f)
+		{
+			elapsed = 0f;
+			transform.position = transform.position + transform.up;
+		}
+	}
+}
